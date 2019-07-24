3b9a56040e270235233e67bb2c4a0e04ebf9332c
Mikhail Isachenko <miha.is@mail.ru>
2019-07-24 15:52:25 +0300

    second

--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+template <class T>
+class Fraction {
+    T x;
+    T y;
+public:
+    Fraction(T x, T y) {
+        this->x = x;
+        this->y = y;
+    }
+    T calculate() { return x/y; }
+    T get_x() { return x; }
+    T get_y() { return y; }
+    void show();
+
+    template <class U>
+    friend ostream &operator << (ostream &out, const Fraction<U> f);
+};
+
+template<class T>
+void Fraction<T>::show() {
+    cout << x << "/" << y;
+}
+
+template<class U>
+ostream &operator << (ostream &out, const Fraction<U> f) {
+    out << f.x << "/" << f.y;
+    return out;
+}
+
+
+int main() {
+    Fraction <int> a(3, 5);
+    Fraction <double> b(3.3, 1.2);
+
+    cout << a << " = " << a.calculate() << endl;
+    cout << b << " = " << b.calculate() << endl;
+
     return 0;
 }
